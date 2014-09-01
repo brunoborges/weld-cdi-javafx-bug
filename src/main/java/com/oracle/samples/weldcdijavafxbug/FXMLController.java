cac9e17bc36abdaa9b107ca18ed8df615aa20f9c
Bruno Borges <bruno.borges@oracle.com>
2014-09-01 17:31:48 -0300

    Project exposing possible bug of FXMLLoader when mixed with CDI (Weld)

---
+++ src/main/java/com/oracle/samples/weldcdijavafxbug/FXMLController.java
@@ -0,0 +1,27 @@
+package com.oracle.samples.weldcdijavafxbug;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class FXMLController implements Initializable {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Controller initialized");
+    }
+}
---
+++ src/main/java/com/oracle/samples/weldcdijavafxbug/MainApp.java
@@ -0,0 +1,46 @@
+package com.oracle.samples.weldcdijavafxbug;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+public class MainApp extends Application {
+
+    private Weld weld;
+    private WeldContainer container;
+
+    @Override
+    public final void init() {
+        weld = new Weld();
+        container = weld.initialize();
+    }
+
+    @Override
+    public final void stop() {
+        weld.shutdown();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmll = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"), null);
+        fxmll.setControllerFactory((clazz) -> container.instance().select(clazz).get());
+        Parent root = fxmll.load();
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
