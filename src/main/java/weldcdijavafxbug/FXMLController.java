83083df5b188a32ac73f81781d1be44a00a3cadb
Bruno Borges <bruno.borges@oracle.com>
2014-09-01 21:36:10 -0300

    Added stacktrace to constructor in controller

--- src/main/java/weldcdijavafxbug/FXMLController.java
+++ src/main/java/weldcdijavafxbug/FXMLController.java
@@ -6,10 +6,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class FXMLController implements Initializable {
+
+    public FXMLController() {
+        System.out.println("This constructur will be called twice! See output");
+        new Exception("STACKTRACE OUTPUT").printStackTrace();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("This method will be called only once!");
+    }
 
     @FXML
     private Label label;
